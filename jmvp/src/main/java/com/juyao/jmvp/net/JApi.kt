dcc3e8c817d38fe85e8d7a8fb8420b9dc33cddfe
juyao <juyao@51nbapi.com>
2018-12-14 10:56:02 +0800

    新增网络请求拦截器

--- jmvp/src/main/java/com/juyao/jmvp/net/JApi.kt
+++ jmvp/src/main/java/com/juyao/jmvp/net/JApi.kt
@@ -1,5 +1,6 @@
 package com.juyao.jmvp.net
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -10,9 +11,13 @@ import java.util.concurrent.TimeUnit
  * Created by juyao on 2017/11/22.
  */
 object JApi {
+    var interceptor: Interceptor?=null
     val DEFAULT_TIMEOUT: Long = 20
     fun getRetrofit(baseUrl: String): Retrofit {
-        val httpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+        val httpClientBuilder: OkHttpClient.Builder = when(interceptor){
+            null->OkHttpClient.Builder()
+            else->OkHttpClient.Builder().addInterceptor(interceptor)
+        }
         httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
         return Retrofit.Builder().
                 client(httpClientBuilder.build()).
@@ -21,6 +26,7 @@ object JApi {
                 addCallAdapterFactory(RxJava2CallAdapterFactory.create()).
                 build()
     }
+
 
 
 }
